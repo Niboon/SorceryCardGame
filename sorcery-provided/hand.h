7694b5453d4420c0e30121a6631553bc57d05700
anmorton <anmorton@student.cs.uwaterloo.ca>
2018-03-29 00:21:27 -0400

    Moved Files

---
+++ sorcery-provided/hand.h
@@ -0,0 +1,18 @@
+#ifndef HAND_H
+#define HAND_H
+#include <utility>
+
+class Card;
+
+class Hand{
+  std::vector<std::unique_ptr<Card>> hand;
+  public:
+  Card takeCard(int which);
+  void addCard(Card newCard);
+  string getDraw();
+  Hand();
+  Hand(Hand &other);
+  ~Hand();
+}
+
+#endif
